2f5deedc944118d96500726f47b8d4dc7df4b996
Woury Diallo <diallowoury@outlook.com>
2018-02-27 23:29:21 +0100

    ProjetStart

---
+++ E-CommerceSite/WouryECommerce/src/main/java/com/example/wouryecommerce/WouryECommerceApplication.java
@@ -0,0 +1,12 @@
+package com.example.wouryecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WouryECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WouryECommerceApplication.class, args);
+	}
+}
